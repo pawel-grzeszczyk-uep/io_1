f26e725312198cbf931c421ce2c27c85e6805c53
pawel_grzeszczyk <p.grzeszczyk00@gmail.com>
2024-03-24 11:05:13 +0100

    Pole trojkata

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ bok_b = 10
 bok_c = 5
 pole_prostokata = bok_b * bok_c
 print(f'pole_prostokata wynosi {pole_prostokata}.')
+
+bok_d = 5
+wysokosc = 6
+pole_trojkata = (bok_d * wysokosc) / 2
+print(f'pole_trojkata wynosi {pole_trojkata}.')
