da68c1fef86d6195deb8cdba2e392ba2b656d6c9
pawel_grzeszczyk <p.grzeszczyk00@gmail.com>
2024-03-24 11:02:23 +0100

    Pole prostokata

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-bok_a = 10.4
-pole_kwadratu = bok_a*bok_a
-print(f'pole_kwadratu: {pole_kwadratu}')
+bok_a = 10
+pole_kwadratu = bok_a * bok_a
+print(f'pole_kwadratu wynosi {pole_kwadratu}.')
+
+bok_b = 10
+bok_c = 5
+pole_prostokata = bok_b * bok_c
+print(f'pole_prostokata wynosi {pole_prostokata}.')
